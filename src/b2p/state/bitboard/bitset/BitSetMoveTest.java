1dd708269d3d2e9b378cd9bbfcd8ed9a72219945
Alessandro Pomponio <pomponio.alessandro@hotmail.it>
2020-04-28 19:22:31 +0200

    Add example test

--- src/b2p/state/bitboard/bitset/BitSetMoveTest.java
+++ src/b2p/state/bitboard/bitset/BitSetMoveTest.java
@@ -1,11 +1,28 @@
 package b2p.state.bitboard.bitset;
 
-import static org.junit.jupiter.api.Assertions.*;
+import b2p.model.Turn;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class BitSetMoveTest {
 
     @org.junit.jupiter.api.Test
     void getMovesForPawn() {
+
+        BitSetState start = new BitSetState();
+        BitSetAction[] wanted = new BitSetAction[]{
+                new BitSetAction("E3", "D3", Turn.WHITE),
+                new BitSetAction("E3", "C3", Turn.WHITE),
+                new BitSetAction("E3", "B3", Turn.WHITE),
+                new BitSetAction("E3", "A3", Turn.WHITE),
+                new BitSetAction("E3", "F3", Turn.WHITE),
+                new BitSetAction("E3", "G3", Turn.WHITE),
+                new BitSetAction("E3", "H3", Turn.WHITE),
+                new BitSetAction("E3", "I3", Turn.WHITE),
+        };
+
+        assertArrayEquals(BitSetMove.getMovesForPawn(BitSetPosition.E3.ordinal(), start).toArray(), wanted);
+
     }
 
     @org.junit.jupiter.api.Test
