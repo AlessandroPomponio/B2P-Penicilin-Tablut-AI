2701ba44eafd7bc19f5736a48adf9ac6879d0bc2
Alessandro Pomponio <pomponio.alessandro@hotmail.it>
2020-04-28 19:06:36 +0200

    Add test stub

---
+++ src/b2p/state/bitboard/bitset/BitSetMoveTest.java
@@ -0,0 +1,14 @@
+package b2p.state.bitboard.bitset;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BitSetMoveTest {
+
+    @org.junit.jupiter.api.Test
+    void getMovesForPawn() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCapturedPawns() {
+    }
+}
