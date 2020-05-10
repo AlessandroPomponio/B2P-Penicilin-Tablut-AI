56a5fde3bc697d6858ab7bd5151576adb160fdff
Alessandro Pomponio <pomponio.alessandro@hotmail.it>
2020-05-10 21:58:58 +0200

    Rename main to Main to follow Java naming convention. Add cli argument parsing

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import b2p.client.B2PTablutClient;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String player = "white";
+        int timeout = 60000;
+
+        if (args.length > 0) {
+            player = args[0];
+        }
+
+        if (args.length > 1) {
+            timeout = Integer.parseInt(args[1]);
+        }
+
+        try {
+            B2PTablutClient client = new B2PTablutClient(player, timeout);
+            client.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
