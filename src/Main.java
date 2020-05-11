da60e74386884edb93eb9c282a5bb850f1b8eb08
Alessandro Pomponio <pomponio.alessandro@hotmail.it>
2020-05-11 23:09:41 +0200

    Fix parameters for competition

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,31 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String player = "black";
-        int timeout = 30000;
+        // The competition rules state that we need 3 input parameters:
+        // player = white | black
+        String player = "white";
 
+        // timeout in seconds (the client constructor wants it in millis)
+        int timeout = 60000;
+
+        // server IP
+        String serverIP = "localhost";
+
+        // Parameters are optional for test purposes
         if (args.length > 0) {
             player = args[0];
         }
 
         if (args.length > 1) {
-            timeout = Integer.parseInt(args[1]);
+            timeout = Integer.parseInt(args[1]) * 1000;
+        }
+
+        if (args.length > 2) {
+            serverIP = args[2];
         }
 
         try {
-            B2PTablutClient client = new B2PTablutClient(player, timeout);
+            B2PTablutClient client = new B2PTablutClient(player, timeout, serverIP);
             client.run();
         } catch (Exception e) {
             e.printStackTrace();
