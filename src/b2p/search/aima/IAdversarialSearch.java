98f0a0809a346e0f38b9671fb26fd07f9dafd821
Alessandro Pomponio <pomponio.alessandro@hotmail.it>
2020-05-17 09:30:16 +0200

    Javadoc update

--- src/b2p/search/aima/IAdversarialSearch.java
+++ src/b2p/search/aima/IAdversarialSearch.java
@@ -2,22 +2,20 @@ package b2p.search.aima;
 
 import b2p.model.IAction;
 import b2p.model.IState;
-import b2p.state.bitboard.bitset.BitSetAction;
-import b2p.state.bitboard.bitset.BitSetState;
 
 /**
- * This interface defines what functions a Class Adversarial Search needs to implement
- *
- * @see <a href="https://github.com/aimacode/aima-java">Aima Java</a>
+ * This interface defines the functions needed to perform an Adversarial Search
  *
  * @author Alessandro Buldini
  * @author Alessandro Pomponio
  * @author Federico Zanini
+ * @see <a href="https://github.com/aimacode/aima-java">Aima Java</a>
  */
 public interface IAdversarialSearch {
 
     /**
      * Returns the action which appears to be the best at the given state
+     *
      * @param state current state of the game, based on which the decision should be made
      * @return the best action to perform for the given heuristic function
      */
@@ -25,7 +23,9 @@ public interface IAdversarialSearch {
 
     /**
      * Returns the metrics of the adversarial search
+     *
      * @return the metrics of the adversarial search
      */
     Metrics getMetrics();
+
 }
